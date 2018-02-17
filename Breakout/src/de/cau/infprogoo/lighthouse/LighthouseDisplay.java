6ccec848ee2907471880dda587dc14bd25db1729
Finite <33845166+finia2NA@users.noreply.github.com>
2018-02-17 19:51:39 +0100

    Data Converter
    Added a converter from 2d Color array to the type of byte array the lighthouse needs.

--- Breakout/src/de/cau/infprogoo/lighthouse/LighthouseDisplay.java
+++ Breakout/src/de/cau/infprogoo/lighthouse/LighthouseDisplay.java
@@ -1,5 +1,7 @@
 package de.cau.infprogoo.lighthouse;
 
+//this is an awt-import. maybe it won't work with fx/swing. 
+import java.awt.Color;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -69,7 +71,11 @@ public class LighthouseDisplay {
 		try {
 			connect("wss://lighthouse.uni-kiel.de/user/" + username + "/model", true);
 		} catch (URISyntaxException e) {
-			throw new InvalidAttributeValueException("Given username is invalid for a connection");
+			// Originally this thing said "Given username is invalid for a connection". But
+			// for some reason eclipse said that there was no Constructer for this exception
+			// type with a string. so I took out the String. not optimal, but a quick fix.
+			// TODO: fix this.
+			throw new InvalidAttributeValueException();
 		}
 	}
 
@@ -119,6 +125,39 @@ public class LighthouseDisplay {
 		if (debugOutput > 0) {
 			System.out.printf("LighthouseDisplay, Connecting to: %s\n", targetUri);
 		}
+	}
+
+	/**
+	 * Converts a xy Color array to the byte array format the lighthouse API
+	 * requires.
+	 * 
+	 * @param colorFrame
+	 *            the xy Color array that represents the current frame.
+	 * @return a byte array representing the input in lighthouse-format.
+	 */
+	static byte[] dataConverter(Color[][] colorFrame) {
+		byte[] data = new byte[1176];
+
+		for (int y = 0; y < 14; y++) {
+			for (int x = 0; x < 28; x++) {
+
+				// The position of the first subpixel of the current pixel in the data array.
+				// For reason for this see Farbmapping.xlsx .
+				int pos = 84 * y + 3 * x;
+
+				// Gets the subpixels of the current pixel. (byte) casts ints from 0 to 255 to
+				// unsigned bytes. which I think is the format we need.
+				byte red = (byte) colorFrame[x][y].getRed();
+				byte green = (byte) colorFrame[x][y].getGreen();
+				byte blue = (byte) colorFrame[x][y].getBlue();
+
+				data[pos] = red;
+				data[pos + 1] = green;
+				data[pos + 2] = blue;
+			}
+		}
+
+		return data;
 	}
 
 	/**
