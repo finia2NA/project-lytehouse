e58f15443b9ba1277e9c29ef01774908ee5265d6
Finite <33845166+finia2NA@users.noreply.github.com>
2018-02-23 15:30:06 +0100

    Multiple balls, getters and setters
    Changed ball to balls-Arraylist so now there can be multiple balls. Also added getters and setters.

--- Breakout/src/net/lighthouse/model/MainModel.java
+++ Breakout/src/net/lighthouse/model/MainModel.java
@@ -3,13 +3,29 @@ package net.lighthouse.model;
 import java.awt.Color;
 import java.util.ArrayList;
 
+/**
+ * the main Model holding all blocks, balls the paddle and all effects.
+ * 
+ * @author finite
+ *
+ */
+
+// TODO: there's a bunch of methods missing right now. The question is wether or
+// not every add/remove/move/changestuff needs its own method in here. I would
+// argue not. as long as you have a way to get the object you want you can just
+// use native object methods.
+
 public class MainModel {
-	ArrayList<BBlock> blocks;
-	BPaddle paddle;
-	BBall ball;
+	private ArrayList<BBlock> blocks;
+	// We'll definitly one have one paddle so this is not an ArrayList.
+	private BPaddle paddle;
+	// IDK, maybe one day we'll have a gamemode/powerup where we'll have multiple
+	// BBalls.
+	private ArrayList<BBall> balls;
+	private ArrayList<multiframe> effects;
 
 	/**
-	 * Creates a Model with all custom data.
+	 * Creates a Model with all custom data. but no starting explosions :(
 	 * 
 	 * @param paddle
 	 *            the paddle.
@@ -20,7 +36,7 @@ public class MainModel {
 	 */
 	public MainModel(BPaddle paddle, BBall ball, ArrayList<BBlock> blocks) {
 		this.paddle = paddle;
-		this.ball = ball;
+		balls.add(ball);
 		this.blocks = blocks;
 	}
 
@@ -28,10 +44,42 @@ public class MainModel {
 	 * Creates a MainModel with preconfigured Blocks, paddle and blocks.
 	 */
 	public MainModel() {
-		int[] atm = {1, 1};
-		ball = new BBall(7, 14, Color.BLUE, 1, atm);
+		int[] atm = { 1, 1 };
+		balls.add(new BBall(7, 14, Color.BLUE, 1, atm));
 		paddle = new BPaddle(7, 27, Color.WHITE, 1);
 		blocks = new ArrayList<BBlock>();
-		
+	}
+
+	public void addExplosion(int x, int y, Color color) {
+		effects.add(new BExplosion(x, y, color, 1));
+
+	}
+
+	public void addObject(BObject object) {
+
+	}
+
+	public ArrayList<BBlock> getBlocks() {
+		return blocks;
+	}
+
+	public int getNumberOfBalls() {
+		return balls.size();
+	}
+
+	public ArrayList<BBall> getAllBalls() {
+		return balls;
+	}
+
+	public BBall getBall(int ballIndex) {
+		return balls.get(ballIndex);
+	}
+
+	public BPaddle getPaddle() {
+		return paddle;
+	}
+
+	public void movePaddle(int paddleNewX) {
+		paddle.setX(paddleNewX);
 	}
 }
