76497454319a62f93725517c76e39197e0986f04
finia2NA <undifini@gmail.com>
2018-02-26 00:48:45 +0100

    Update Paddle Position on MouseMoved

--- Breakout/src/net/lighthouse/controller/MainController.java
+++ Breakout/src/net/lighthouse/controller/MainController.java
@@ -3,6 +3,7 @@ package net.lighthouse.controller;
 import acm.program.GraphicsProgram;
 import net.lighthouse.model.MainModel;
 import net.lighthouse.view.MainView;
+import java.awt.event.*;
 
 /**
  * Wir brauchen sowieso die Mausevents, deshalb mache ich das Ding jetzt mal zu
@@ -18,9 +19,20 @@ public class MainController extends GraphicsProgram {
 
 		view = new MainView(this);
 		view.init();
+		addMouseListeners();
 	}
 
 	public void run() {
+		view.refresh(model);
+	}
+
+	public void mouseMoved(MouseEvent e) {
+		model.movePaddle(e.getX() - 80);
+	}
+
+	// until we have automatic refreshs you can refresh the view by clicking
+	// anywhere.
+	public void mouseClicked(MouseEvent e) {
 		view.refresh(model);
 	}
 }
