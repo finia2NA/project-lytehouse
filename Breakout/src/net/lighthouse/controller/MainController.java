b899c074656c35c4f73a9bc08a076821130b42b3
Christoph Fricke <christoph@frickeonline.de>
2018-02-26 22:01:09 +0100

    Start of Collision detection...

--- Breakout/src/net/lighthouse/controller/MainController.java
+++ Breakout/src/net/lighthouse/controller/MainController.java
@@ -2,9 +2,11 @@ package net.lighthouse.controller;
 
 import acm.program.GraphicsProgram;
 import net.lighthouse.collision.CollisionChecker;
+import net.lighthouse.model.BBlock;
 import net.lighthouse.model.MainModel;
 import net.lighthouse.settings.Settings;
 import net.lighthouse.view.MainView;
+
 import java.awt.event.*;
 
 /**
@@ -15,10 +17,10 @@ public class MainController extends GraphicsProgram {
     private MainModel model;
     private CollisionChecker ballChecker;
 
-	public void init() {
-		Settings.readUserSettings("settings.txt");
-		// initializes the Model with default values(ball, paddle, buch o' blocks)
-		model = new MainModel();
+    public void init() {
+        Settings.readUserSettings("settings.txt");
+        // initializes the Model with default values(ball, paddle, buch o' blocks)
+        model = new MainModel();
 
         ballChecker = new CollisionChecker(model.getBall(0));
 
@@ -30,31 +32,35 @@ public class MainController extends GraphicsProgram {
         model.getBall(0).setSpeed(speed);
     }
 
-	/**
-	 * I need this to attach a debugger with IntelliJ. Otherwise IntelliJ is not
-	 * able to find a main method while using the acm library. See more:
-	 * https://stackoverflow.com/questions/28058665/java-runtime-error-could-not-initialize-class-formpreviewframe
-	 *
-	 * @param args
-	 *            Runtime arguments
-	 */
-	public static void main(String[] args) {
-		new MainController().start(args);
-	}
+    /**
+     * I need this to attach a debugger with IntelliJ. Otherwise IntelliJ is not
+     * able to find a main method while using the acm library. See more:
+     * https://stackoverflow.com/questions/28058665/java-runtime-error-could-not-initialize-class-formpreviewframe
+     *
+     * @param args Runtime arguments
+     */
+    public static void main(String[] args) {
+        new MainController().start(args);
+    }
 
-	public void run() {
-		view.refresh(model);
+    public void run() {
+        view.refresh(model);
 
-		// Game Loop
+        // Game Loop
         boolean runGame = true;
-		long previousRefreshTime = System.currentTimeMillis();
-		while (runGame) {
-			long nextTime = System.currentTimeMillis();
+        long previousRefreshTime = System.currentTimeMillis();
+        while (runGame) {
+            long nextTime = System.currentTimeMillis();
 
             // 1s == 1000ms => 50fps == 1/50s == 20ms
             if (nextTime - previousRefreshTime > 20) {
                 ballChecker.handlePaddleCollision(model.getPaddle());
                 runGame = ballChecker.handleBorderCollision(this.getWidth(), model.getPaddle().getY());
+                BBlock[] blocksToHide = ballChecker.handleBlockCollision(model.getBlocks());
+
+                for (BBlock block : blocksToHide) {
+                    block.setOpacity(0.00d);
+                }
 
                 model.getBall(0).move();
                 view.refresh(model);
@@ -64,7 +70,7 @@ public class MainController extends GraphicsProgram {
         System.out.println("You lost!");
     }
 
-	public void mouseMoved(MouseEvent e) {
-		model.movePaddle(e.getX() - model.getPaddle().getWith() / 2);
-	}
+    public void mouseMoved(MouseEvent e) {
+        model.movePaddle(e.getX() - model.getPaddle().getWith() / 2);
+    }
 }
