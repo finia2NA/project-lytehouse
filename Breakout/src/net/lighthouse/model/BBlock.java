d0b82e2c0ba0ec27db158ee86e169474b8db1178
Finite <33845166+finia2NA@users.noreply.github.com>
2018-03-02 23:04:53 +0100

    WHAT IS MY PURPOSE?
    You are a BObject.

--- Breakout/src/net/lighthouse/model/BBlock.java
+++ Breakout/src/net/lighthouse/model/BBlock.java
@@ -6,12 +6,10 @@ public class BBlock extends BObject {
 
 	public BBlock(int x, int y, int width, int height) {
 		super(x, y, width, height);
-		// TODO Auto-generated constructor stub
 	}
 
 	public BBlock(int x, int y, int width, int height, Color color) {
 		super(x, y, width, height, color);
-		// TODO Auto-generated constructor stub
 	}
 
 	// Diese Klasse existiert nur damit die View instanceOf BBlock sagen kann.
