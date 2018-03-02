fb33a1fae3dd3ff80cd3651ba5ad66dc3f09568b
Christoph Fricke <christoph@frickeonline.de>
2018-03-02 11:26:02 +0100

    Updated Javadoc for opacity

--- Breakout/src/net/lighthouse/model/Renderable.java
+++ Breakout/src/net/lighthouse/model/Renderable.java
@@ -10,32 +10,30 @@ import net.lighthouse.view.BPixel;
  * BObjects do that, but maybe one day there'll be a day where we need a
  * non-Renderable BObject. And that day will be great bc then we can say that
  * there was a reason why we didn't just write these methods into BObject.
- * 
- * @author undif
  *
+ * @author undif
  */
 public interface Renderable {
 
-	/**
-	 * sets the Color of an Object
-	 */
-	public void setColor(Color color);
+    /**
+     * sets the Color of an Object
+     */
+    public void setColor(Color color);
 
-	/**
-	 * returns the color of the Object.
-	 * 
-	 * @return the color of the Object.
-	 */
-	public Color getColor();
+    /**
+     * returns the color of the Object.
+     *
+     * @return the color of the Object.
+     */
+    public Color getColor();
 
-	/**
-	 * sets the opacity of an object to true or false.
-	 * 
-	 * @param visibility
-	 *            {@code 0} object is invisible, {@code 100} object is visible
-	 */
-	public void setOpacity(double opacity);
-	
-	public double getOpacity();
+    /**
+     * sets the opacity of an object.
+     *
+     * @param opacity {@code 0} object is invisible, {@code 1} object is visible
+     */
+    public void setOpacity(double opacity);
+
+    public double getOpacity();
 
 }
