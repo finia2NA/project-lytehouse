2968a25bc74f7294f2ebcf741d8426694f2966fe
Christoph Fricke <christoph@frickeonline.de>
2018-03-06 18:28:29 +0100

    Makes boss and laser working

--- Breakout/src/net/lighthouse/controller/BBossController.java
+++ Breakout/src/net/lighthouse/controller/BBossController.java
@@ -27,10 +27,18 @@ public class BBossController {
             throw new IllegalStateException("Boss should be death by now!");
         }
 
+        BLaser laser = null;
+
         // Changes the color of the boss for one frame if damage was dealt.
         if (boss.getHealth() != boss.getOldHealth()) {
             boss.setColor(Color.WHITE);
             boss.evenOldHealth();
+
+            RandomGenerator rnd = RandomGenerator.getInstance();
+            int[] speed = {rnd.nextInt(-4, 4), rnd.nextInt(4, 6)};
+
+            // Spawns a new laser at the bottom center of the boss
+            laser = new BLaser(boss.getX() + boss.getWith() / 2, boss.getY() + boss.getHeight(), speed);
         } else {
             boss.setColor(Color.GREEN);
         }
@@ -38,17 +46,6 @@ public class BBossController {
         // Indicates that the boss is nearly dead.
         if (boss.getHealth() == 1) {
             boss.setColor(Color.RED);
-        }
-
-        BLaser laser = null;
-
-        // We want to spawn a new laser if boss health is not even
-        if (boss.getHealth() % 2 != 0) {
-            RandomGenerator rnd = RandomGenerator.getInstance();
-            int[] speed = {rnd.nextInt(-4, 4), rnd.nextInt(4, 6)};
-
-            // Spawns a new laser at the bottom center of the boss
-            laser = new BLaser(boss.getX() + boss.getWith() / 2, boss.getY() + boss.getHeight(), speed);
         }
 
         return laser;
