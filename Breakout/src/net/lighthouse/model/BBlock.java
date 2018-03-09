6cef41a867ec92e216b57042d0fafeba5ec36711
Christoph Fricke <christoph@frickeonline.de>
2018-03-09 11:19:55 +0100

    Removes BBlock constructor without a color

--- Breakout/src/net/lighthouse/model/BBlock.java
+++ Breakout/src/net/lighthouse/model/BBlock.java
@@ -10,16 +10,6 @@ import java.awt.Color;
  */
 public class BBlock extends BObject {
 
-	/**
-	 * Constructs a new BBlock.
-	 * @param x the xpos of the Block (leftmost).
-	 * @param y the ypos of the Block (upper).
-	 * @param width the width of the Block.
-	 * @param height the height of the Block.
-	 */
-	public BBlock(int x, int y, int width, int height) {
-		super(x, y, width, height);
-	}
 
 	public BBlock(int x, int y, int width, int height, Color color) {
 		super(x, y, width, height, color);
